92d8607724033d98ae170bbdea2c4b857f8a4ea6
thlohith <thlohith@din84000612.corp.capgemini.com>
2021-12-23 21:47:42 +0530

    sales class dispaly method added with sum

--- sampledemo/src/main/java/sampledemo/salesclass.java
+++ sampledemo/src/main/java/sampledemo/salesclass.java
@@ -2,10 +2,16 @@ package sampledemo;
 
 public class salesclass {
   public void display() {
+
 	  int[] arr= {22,44,78,56};
-	  for(int num:arr)
-		  System.out.println(arr);
+	  int sum=0;
+	  for(int num:arr){
+		  sum+=num;
+		  System.out.println(num);
   }
+	  System.out.println("summation:" + sum);
+	   
+	  }
   public void print() {
 	  System.out.println("print all array numbers");
 	  display();
