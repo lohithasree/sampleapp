4a4d0a4e3fa3dcfa5f70622e66ed062e9bc21f9a
thlohith <thlohith@din84000612.corp.capgemini.com>
2021-12-23 21:58:28 +0530

    dispaly method updated in sales class

--- sampledemo/src/main/java/sampledemo/salesclass.java
+++ sampledemo/src/main/java/sampledemo/salesclass.java
@@ -3,9 +3,10 @@ package sampledemo;
 public class salesclass {
   public void display() {
 	  int[] arr= {22,44,78,56};
-	  for(int num:arr)
+	  for(int i=0;i<arr.length;i++)
 		  System.out.println(arr);
-  }
+	  System.out.println("array length" + arr.length);
+  } 
   public void print() {
 	  System.out.println("print all array numbers");
 	  display();
