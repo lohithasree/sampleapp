6e237cb6abe0fd4b1fe5dc7ec04a09a11df33ccc
thlohith <thlohith@din84000612.corp.capgemini.com>
2021-12-23 18:58:56 +0530

    first commit !!

---
+++ sampledemo/src/main/java/sampledemo/MainClass.java
@@ -0,0 +1,14 @@
+package sampledemo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("hello world");
+	System.out.println("hello");
+	System.out.println("have a good day");
+	System.out.println("hellohiiworld");
+	System.out.println("hiii");
+	}
+
+}
