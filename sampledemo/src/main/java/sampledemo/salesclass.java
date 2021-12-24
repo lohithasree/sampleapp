48003bada4cb3efc533daf006b6ac99f7254d27f
thlohith <thlohith@din84000612.corp.capgemini.com>
2021-12-24 15:57:34 +0530

    print method updated

--- sampledemo/src/main/java/sampledemo/salesclass.java
+++ sampledemo/src/main/java/sampledemo/salesclass.java
@@ -3,16 +3,18 @@ package sampledemo;
 public class salesclass {
   public void display() {
 	  int[] arr= {22,44,78,56};
-	  int sum=0;
-	  for(int i=0;i<arr.length;i++) {
-		  sum=+arr[i];
-		  System.out.println(arr[i]);
+          int sum=0;
+	  for(int num:arr) {
+                  sum+=num;
+		  System.out.println(num);
 	  }
-	  System.out.println("arraysize " + arr.length);
-	  System.out.println("summation:" + sum);
+		
+	  System.out.println("summation " + sum);
   }
   public void print() {
 	  System.out.println("print all array numbers");
+          for(int i=0;i<100;i++)
+          System.out.println("print i" + i);
 	  display();
   }
 }
