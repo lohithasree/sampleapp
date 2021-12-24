573d18b0eecae03dbb84708c5af07574d5f20032
thlohith <thlohith@din84000612.corp.capgemini.com>
2021-12-24 15:36:22 +0530

    display method updated in sales class

--- sampledemo/src/main/java/sampledemo/salesclass.java
+++ sampledemo/src/main/java/sampledemo/salesclass.java
@@ -3,12 +3,10 @@ package sampledemo;
 public class salesclass {
   public void display() {
 	  int[] arr= {22,44,78,56};
-	  int sum=0;
-	  for(int num:arr) {
-		  sum=+num;
-		  System.out.println(arr);
-		  }
-	  System.out.println("Summation " + sum);
+	  for(int i=0;i<arr.length;i++) 
+		  System.out.println(arr[1]);
+		
+	  System.out.println("arraysize " + arr.length);
   }
   public void print() {
 	  System.out.println("print all array numbers");
