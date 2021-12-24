112f2960bbfe835e2295e52a486fee5ab7085a3f
thlohith <thlohith@din84000612.corp.capgemini.com>
2021-12-24 15:25:26 +0530

    in display summation is added

--- sampledemo/src/main/java/sampledemo/salesclass.java
+++ sampledemo/src/main/java/sampledemo/salesclass.java
@@ -3,8 +3,12 @@ package sampledemo;
 public class salesclass {
   public void display() {
 	  int[] arr= {22,44,78,56};
-	  for(int num:arr)
+	  int sum=0;
+	  for(int num:arr) {
+		  sum=+num;
 		  System.out.println(arr);
+		  }
+	  System.out.println("Summation " + sum);
   }
   public void print() {
 	  System.out.println("print all array numbers");
